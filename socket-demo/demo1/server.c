76ba86c55b17d7efb694b4fc1e2d6226ef37230f
Your Name <you@example.com>
2017-08-21 19:04:36 +0800

    upate

---
+++ socket-demo/demo1/server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
